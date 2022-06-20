dfb2bfc4671626bbac3d331d2c07c2c4f6a99058
cjf646 <ficormusic@yahoo.ca>
2022-06-19 22:03:22 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,156 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+
+# class MyGrid(Widget):
+#     pass
+
+
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+
+        self.inside = GridLayout()
+        self.inside.cols = 2
+
+        self.cols = 2
+        self.inside.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline=False)
+        self.inside.add_widget(self.name)
+
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.lastName = TextInput(multiline=False)
+        self.inside.add_widget(self.lastName)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.submit.bind(on_press=self.pressed)
+        self.add_widget(self.submit)
+
+    def pressed(self, instance):
+        name = self.name.text
+        last = self.lastName.text
+        email = self.email.text
+        print("Name:", name, "Last Name:", last, "Email:", email)
+        self.name.text = ""
+        self.lastName.text = ""
+        self.email.text = ""
+        print("Pressed")
+
+
+# class MyApp(App):
+#     def build(self):
+#         return MyGrid()
+#         # return Label(text="Tech with Tim")
+#
+#
+# if __name__ == "__main__":
+#     MyApp().run()
+
+# import speech_recognition as sr
+# import speech_rec as s
+#
+#
+#
+#
+#
+# #
+# def main():
+#     r = sr.Recognizer()
+#     m = sr.Microphone()
+#
+#     r = sr.Recognizer()
+#     on = True
+#     while on:
+#         with sr.Microphone() as source:
+#             audio = r.listen(source)
+#
+#             try:
+#                 text = r.recognize_google(audio)
+#                 print("You said: {}".format(text))
+#             except:
+#                 print("Sorry, we did not recognize your voice")
+# #
+#
+#
+#
+#
+#     r = sr.Recognizer()
+#     my_mic = sr.Microphone(device_index=1) #my device index is 1, you have to put your device index
+#
+#     with my_mic as source:
+#
+#         print("Say now!!!!")
+#
+#         audio = r.listen(source) #take voice input from the microphone
+#     text = r.recognize_google(audio)
+#     print(text) #to print voice into text
+#     sent = s.getPolarity(text)
+#     sentiment = s.getAnalysis(sent)
+#
+#     print(sentiment)
+#
+#
+#     r = sr.Recognizer()
+#     mic = sr.Microphone()
+#     with mic as source:
+#         r.adjust_for_ambient_noise(source)
+#         audio = r.listen(source)
+#         transcript = r.recognize_google(audio)
+#         print(transcript)
+#
+#
+#
+# if __name__ == "__main__":
+#     main()
+#
+#
+# Python Program that helps translate Speech to Text
+#
+# The Recognizer is initialized.
+# UserVoiceRecognizer = sr.Recognizer()
+#
+# while (1):
+#     try:
+#
+#         with sr.Microphone() as source:
+#
+#             UserVoiceRecognizer.adjust_for_ambient_noise(source, duration=0.5)
+#             # speech_recognition.dynamic_threshold = True
+#             # The Program listens to the user voice input.
+#
+#             UserVoiceInput = UserVoiceRecognizer.listen(source)
+#
+#             UserVoiceInput_converted_to_Text = UserVoiceRecognizer.recognize_google(UserVoiceInput)
+#
+#
+#             UserVoiceInput_converted_to_Text = UserVoiceInput_converted_to_Text.lower()
+#
+#             sent = s.getPolarity(UserVoiceInput_converted_to_Text)
+#             sentiment = s.getAnalysis(sent)
+#
+#
+#
+#             print(UserVoiceInput_converted_to_Text)
+#             print(sentiment)
+#
+#
+#     except KeyboardInterrupt:
+#         print('A KeyboardInterrupt encountered; Terminating the Program !!!')
+#         exit(0)
+#
+#     except sr.UnknownValueError:
+#         print("No User Voice detected OR unintelligible noises detected OR the recognized audio cannot be matched to text !!!")
+#
---
+++ speech_rec.py
@@ -0,0 +1,456 @@
+
+
+#library that contains punctuation
+import string
+string.punctuation
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from textblob import TextBlob
+import pickle
+
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+stemmer = PorterStemmer()
+import time
+import speech_recognition as sr
+import pyttsx3 as s
+
+
+import nltk
+# nltk.download()
+import pandas as pd
+import numpy as np
+import re
+from transformers import pipeline
+
+# data = pd.read_csv("tweet_emotions.csv")
+# # sent = pd.DataFrame(data)
+# # print("Original DataFrame:\n", data)
+# # df = pd.DataFrame(data)
+#
+# sent = data[data['sentiment'].between(0, 2)].copy()
+# print("sentiment: ")
+# print(sent)
+#
+# # def remove_punctuation(txt):
+# #     txt_nopunct = "".join([c for c in txt if c not in string.punctuation]) #checking if belongs to punctuation or not
+# #     return txt_nopunct
+# #
+# # sent['content'] = sent['content'].apply(remove_punctuation)
+# #
+# # sent['content'] = sent["content"].str.lower()
+# #
+# # def tokenize(text):
+# #     text_tokenized = text.split()
+# #     return text_tokenized
+# #
+# # sent['content'] = sent['content'].apply(tokenize)
+# #
+# #
+#
+#
+#
+# # def stopword_removal(stop):
+# #     en_stops = set(stopwords.words('english'))
+# #     new_list = [word for word in stop if word not in en_stops]
+# #     return new_list
+# #
+# # sent['content'] = sent['content'].apply(stopword_removal)
+#
+#
+# # def remove_punc(txt):
+# #     txt_nopunct = " ".join([c for c in txt if c not in string.punctuation]) #checking if belongs to punctuation or not
+# #     return txt_nopunct
+#
+# # sent['content'] = sent['content'].apply(remove_punc)
+# #
+# # def remove_numbers(text):
+# #     result = re.sub(r'\d+', '', text)
+# #     return result
+# #
+# #
+# # sent['content'] = sent['content'].apply(remove_numbers)
+#
+# # def stem_words(text):
+# #     word_tokens = word_tokenize(text)
+# #     stems = [stemmer.stem(word) for word in word_tokens]
+# #     return stems
+# #
+# # sent['content'] = sent['content'].apply(stem_words)
+# #
+# #
+# # sent['content'] = sent['content'].apply(remove_punc)
+#
+# print(sent)
+#
+#
+# def getSubjectivity(text):
+#     return TextBlob(text).sentiment.subjectivity
+#
+# #     # Create a function to get the polarity
+
+# def callback(recognizer, audio):
+#     print(recognizer.recognize_google(audio))
+#
+# recognizer = sr.Recognizer()
+# mic = sr.Microphone()
+#
+# with mic as source:
+#     recognizer.adjust_for_ambient_noise(source)
+#
+# stop_listening = recognizer.listen_in_background(mic, callback)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# sent['TextBlob_Polarity'] = sent['content'].apply(getPolarity).apply(getAnalysis)
+# sent['Results'] = sent.TextBlob_Polarity == sent.sentiment
+#
+#
+# print(sent)
+#
+#
+# print(sent['Results'].value_counts())
+# print(sent['Results'].value_counts() / sent.shape[0])
+#
+#
+# some_value = False
+#
+# false = sent[sent['Results'] == some_value]
+#
+# print(false)
+#
+# print(false['content'])
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+# def text_proc(txt):
+#     proc = TextBlob()
+#     x = proc.sentiment.polarity
+#     if x < 0:
+#         n = "Negative"
+#         return n
+#     elif x == 0:
+#         neutral = "Neutral"
+#         return neutral
+#     elif (x > 0) & (x <= 1):
+#         p = "Positive"
+#         return p
+
+
+# data['sentiment_test'] = data.apply(text_proc['content'])
+#
+# print(data)
+
+
+
+#
+# for x in range(10):
+#     y = input("Type Sentence: ")
+#     proc = TextBlob(y)
+#     x = proc.sentiment.polarity
+#
+#     if x < 0:
+#         print("Negative")
+#     elif x == 0:
+#         print("Neutral")
+#     elif (x > 0) & (x <= 1):
+#         print("Positive")
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# import nltk
+# # nltk.download()
+# import pandas as pd
+# import numpy as np
+#
+# # from transformers import pipeline
+#
+# data = pd.read_csv("tweet_emotions.csv")
+#
+# read = data.head()
+# print(read)
+#
+# text = data["content"]
+#
+# def remove_punctuation(txt):
+#     txt_nopunct = "".join([c for c in txt if c not in string.punctuation]) #checking if belongs to punctuation or not
+#     return txt_nopunct
+#
+# data['content_clean'] = data['content'].apply(remove_punctuation)
+# print(data.head())
+#
+# data["lower_case"] = data["content_clean"].str.lower()
+#
+# print(data.head())
+# #
+# def tokenize(text):
+#     text_tokenized = text.split()
+#     return text_tokenized
+#
+# data['tokenized'] = data['lower_case'].apply(tokenize)
+# print(data.head())
+#
+# def stopword_removal(stop):
+#     en_stops = set(stopwords.words('english'))
+#     new_list = [word for word in stop if word not in en_stops]
+#     return new_list
+#
+# data['meaningful_words'] = data['tokenized'].apply(stopword_removal)
+# print(data.head())
+#
+# print(stopwords.words('english'))
+
+
+
+# for column in data:
+# # #     # # Select column contents by column
+# # #     # # name using [] operator
+# # #     # columnSeriesObj = stu_df[column]
+# # #     # print('Column Name : ', column)
+# # #     # print('Column Contents : ', columnSeriesObj.values)
+# #
+# #
+#     data["tokenization"] = data["lower_case"].column.split()
+#     print(data.head())
+# print(data.head())
+#     proc = column.split()
+#     print(proc.head())
+def getPolarity(text):
+    return TextBlob(text).sentiment.polarity
+
+
+# def getAnalysis(score):
+#     if score < -0.4:
+#         return 1
+#     elif (score > -0.4) & (score < 0.4):
+#         return 2
+#     else:
+#         return 0
+def getAnalysis(score):
+    if score < 0:
+        return 'Negative'
+    elif score == 0:
+        return 'Neutral'
+    elif (score > 0) & (score <= 1):
+        return 'Positive'
+score = 0
+def positivity_count(text, score):
+    if text > 0:
+        final_score = score + 1
+        return final_score
+    elif text < 0:
+        final_score = score - 1
+        return final_score
+
+    elif text == 0:
+        final_score = score
+        return final_score
+
+def getSubjectivity(text):
+    return TextBlob(text).sentiment.subjectivity
+    # if subj > 0.5:
+    #     return "Objective"
+    # elif (subj > 0) & (subj < 0.5):
+    #     return "Subjective"
+
+
+def get_Response(sentiment):
+    if sentiment == 'Positive':
+        audio = s.init()
+        rate = audio.getProperty('rate')
+# print(rate)
+        audio.setProperty('rate', 150)
+
+        volume = audio.getProperty('volume')
+# print(volume)
+
+        audio.setProperty('volume', 0.5)
+
+        audio.say("That is so fucking nice. oh my god. I love you")
+        audio.runAndWait()
+    elif sentiment == 'Negative':
+        audio = s.init()
+        rate = audio.getProperty('rate')
+        # print(rate)
+        audio.setProperty('rate', 150)
+
+        volume = audio.getProperty('volume')
+        # print(volume)
+
+        audio.setProperty('volume', 0.5)
+
+        audio.say("That is so fucking rude. Smarten up. You are now at: ")
+        audio.runAndWait()
+# r = sr.Recognizer()
+
+def check_Sentiment(sentiment, human_sentiment):
+    if sentiment == human_sentiment:
+        return 'GOOD'
+    elif sentiment != human_sentiment:
+        return human_sentiment
+
+# def saveDataframe(df, index):
+#     df.at[index, 'Text'] = text
+#     df.at[index, 'Sentiment'] = human_sentiment
+#     # print(df)
+#     index = index + 1
+#     df.to_pickle("a_file.pkl")
+#     return pd.read_pickle("a_file.pkl")
+
+import time
+import speech_recognition as s_r
+
+# print(pd.read_pickle("a_file.pkl"))
+
+df = pd.DataFrame(columns=['Text', 'Sentiment'])
+# df = pd.DataFrame(columns=['Text', 'Sentiment'])
+index = 0
+    # print(s_r.__version__) # just to print the version not required
+r = s_r.Recognizer()
+my_mic = s_r.Microphone(device_index=1) #my device index is 1, you have to put your device index
+
+with my_mic as source:
+
+    # r.adjust_for_ambient_noise(source)
+
+    for x in range(3):
+        print("Say now!!!!")
+        audio = r.listen(source, phrase_time_limit=5)
+    # audio = r.listen(source) #take voice input from the microphone
+        text = r.recognize_google(audio)
+        print(text) #to print voice into text
+        sent = getPolarity(text)
+        sentiment = getAnalysis(sent)
+        subjective = getSubjectivity(text)
+
+        count = positivity_count(sent, score)
+        score = count
+
+
+        print("The text is: ", subjective)
+        print(sentiment)
+
+        human_sentiment = input("what is sentiment: ")
+
+        # print(human_sentiment)
+
+        check = check_Sentiment(sentiment, human_sentiment)
+        if check == 'GOOD':
+            print("Correct sentiment")
+        elif check != 'GOOD':
+            print('Correct sentiment is:', check)
+            # df.read_pickle("a_file.pkl")
+
+            df['Text'] = [text]
+            df['Sentiment'] = [human_sentiment]
+
+            df.at[index, 'Text'] = text
+            df.at[index, 'Sentiment'] = human_sentiment
+
+            # print(df)
+            index = index + 1
+            df.to_pickle("a_file.pkl")
+
+
+
+            # print(output)
+
+
+            # df.to_csv("df.csv")
+            # df = df.index
+
+        print("Your score is at: ", count)
+        response = get_Response(sentiment)
+        print(pd.read_pickle("a_file.pkl"))
+        # print(output)
+    # saveDataframe(df)
+    # print(saveDataframe(df))
+# df.to_pickle("a_file.pkl")
+
+# print("Dataframe Contents ", df, sep='\n')
+# import speech_recognition as sr
+
+
+
+# audio = s.init()
+#
+# rate = audio.getProperty('rate')
+# # print(rate)
+# audio.setProperty('rate', 150)
+#
+# volume = audio.getProperty('volume')
+# # print(volume)
+#
+# audio.setProperty('volume', 0.5)
+#
+# audio.say("Hello world")
+# audio.runAndWait()
+#
+# r = sr.Recognizer()
+
+# def SpeakText(command):
+#
+#     engine = s.init()
+#     engine.say(command)
+#     engine.runAndWait()
+#
+#     SpeakText("Hello World")
